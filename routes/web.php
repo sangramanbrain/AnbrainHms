4893c10081a2ca07bafc9357cc7ea7f31d62391d
unknown <sangram@anbrain.in>
2021-12-01 17:42:10 +0530

    add ///

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::get('/My-Account', [HomeController::Class, 'index']);
+
+///////////////////
