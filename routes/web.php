f61f9c9601e6a0937714465990e77a8bbbf3f232
uday <udaykiran17101999@gmail.com>
2021-12-01 11:29:45 +0530

    admin layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::view('admin/dashboard','admin.dashboard');
+
+Route::view('admin/profile','admin.profile');
